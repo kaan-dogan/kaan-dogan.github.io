0066d5cc47f161c262570978949a56ee347e4e1d
kaan-dogan <kaandn51@gmail.com>
2023-11-17 20:33:36 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var btn = document.getElementById("goTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+}
+
